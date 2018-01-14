abd5bd0cf6fd339af8b18dabf0deda2a531deba2
qtotuan <quynh.totuan@gmail.com>
2018-01-14 12:35:01 +0100

    Fix naming of puzzle migration

--- db/migrate/20180113214027_create_puzzles.rb
+++ db/migrate/20180113214027_create_puzzles.rb
@@ -1,4 +1,4 @@
-class CreatePuzzle < ActiveRecord::Migration[5.1]
+class CreatePuzzles < ActiveRecord::Migration[5.1]
   def change
     create_table :puzzles do |t|
       t.string :title, null: false
