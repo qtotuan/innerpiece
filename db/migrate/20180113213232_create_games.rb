822e44ddc3dc6c5e858ca33dbc7baa8187af3b0c
ahamouda <ahmed.hamouda8888@gmail.com>
2018-01-14 12:17:23 +0100

    add migrate files for games and puzzle

--- db/migrate/20180113213232_create_games.rb
+++ db/migrate/20180113213232_create_games.rb
@@ -2,6 +2,9 @@ class CreateGames < ActiveRecord::Migration[5.1]
   def change
     create_table :games do |t|
       t.boolean :completed
+      t.references :user
+      t.references :puzzle
+      t.references :size
     end
   end
 end
--- db/migrate/20180113214027_create_puzzle.rb
+++ db/migrate/20180113214027_create_puzzle.rb
@@ -1,6 +1,8 @@
 class CreatePuzzle < ActiveRecord::Migration[5.1]
   def change
     create_table :puzzles do |t|
+      t.string :title, null: false
+      t.references :category
     end
   end
 end
