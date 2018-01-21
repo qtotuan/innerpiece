573c63cdf01ecc25732f7e3204d160fc71f2a89d
qtotuan <quynh.totuan@gmail.com>
2018-01-21 18:59:17 +0100

    updating routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,8 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :sessions, only: [:create, :new, :destroy]
+  get '/signup', to: 'users#new'
+  get '/logout', to: 'sessions#destroy'
+  get '/login', to: 'sessions#new'
+  post '/login', to: 'session#create'
 end
