487b1d405e3402905224dcd8c2d70e511c1ca89d
qtotuan <quynh.totuan@gmail.com>
2018-01-27 10:39:38 +0100

    fix login bug, and import bootstrap

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -6,14 +6,13 @@ class SessionsController < ApplicationController
   end
 
   def create
-    puts 'create method!'
     user = User.find_by_email(params[:email])
     if user && user.authenticate(params[:params[:password]])
       session[:user_id] = user.id
       redirect_to root_path, :notice => 'Welcome back, #{user.email}'
     else
       flash.now.alert = 'Invalid email or password'
-      render :new
+      redirect_to login_path
     end
   end
 
